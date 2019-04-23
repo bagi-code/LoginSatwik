9c08a484ff11fa8f6ec5a14cd4771ac845aaf981
robbydianputra <bagicode@gmail.com>
2019-04-23 19:42:46 +0700

    Tutorial Bagian 4 : Register Page

---
+++ app/src/main/java/com/bagicode/loginsatwik/RegisterActivity.java
@@ -0,0 +1,13 @@
+package com.bagicode.loginsatwik;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
