cf8d74138eea46bb3c8bd605c6fe069d1b3993c0
robbydianputra <bagicode@gmail.com>
2019-04-24 20:19:33 +0700

    Tutorial Bagian 5 : Intent

---
+++ app/src/main/java/com/bagicode/loginsatwik/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.bagicode.loginsatwik;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button btnRegister = findViewById(R.id.btn_register);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goRegister = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(goRegister);
+                finish();
+
+            }
+        });
+
+
+    }
+
+}
--- app/src/main/java/com/bagicode/loginsatwik/RegisterActivity.java
+++ app/src/main/java/com/bagicode/loginsatwik/RegisterActivity.java
@@ -1,7 +1,10 @@
 package com.bagicode.loginsatwik;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goLogin = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(goLogin);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent goLogin = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(goLogin);
+        finish();
     }
 }
