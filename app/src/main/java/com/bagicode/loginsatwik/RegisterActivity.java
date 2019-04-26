2bfa04d4c2909920b128953e0d0eecc991381abd
robbydianputra <bagicode@gmail.com>
2019-04-26 19:56:59 +0700

    Tutorial Bagian 7 : Firebase signup by email

--- app/src/main/java/com/bagicode/loginsatwik/RegisterActivity.java
+++ app/src/main/java/com/bagicode/loginsatwik/RegisterActivity.java
@@ -2,16 +2,35 @@ package com.bagicode.loginsatwik;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText etUsername, etPassowrd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        etUsername = findViewById(R.id.et_username);
+        etPassowrd = findViewById(R.id.et_password);
 
         Button btnLogin = findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(new View.OnClickListener() {
@@ -22,6 +41,50 @@ public class RegisterActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        FloatingActionButton fabRegister = findViewById(R.id.fab_register);
+        fabRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String username = etUsername.getText().toString();
+                String password = etPassowrd.getText().toString();
+
+                if (username.equals("")) {
+                    Toast.makeText(RegisterActivity.this, "Silahkan isi username Anda",
+                            Toast.LENGTH_SHORT).show();
+                } else if (password.equals("")) {
+                    Toast.makeText(RegisterActivity.this, "Silahkan isi password Anda",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+
+                    mAuth.createUserWithEmailAndPassword(username, password)
+                            .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        FirebaseUser user = mAuth.getCurrentUser();
+
+                                        Toast.makeText(RegisterActivity.this, "Authentication success.",
+                                                Toast.LENGTH_SHORT).show();
+
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
+
+                                    }
+
+                                    // ...
+                                }
+                            });
+                }
+
+            }
+        });
+
+
     }
 
     @Override
@@ -29,5 +92,12 @@ public class RegisterActivity extends AppCompatActivity {
         Intent goLogin = new Intent(RegisterActivity.this, LoginActivity.class);
         startActivity(goLogin);
         finish();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
     }
 }
