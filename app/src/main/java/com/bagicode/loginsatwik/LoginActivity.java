755a660d5c2bdb650d6ecc251ac655822025f781
robbydianputra <bagicode@gmail.com>
2019-04-27 20:21:12 +0700

    Tutorial Bagian 8 : Firebase sigin by email

--- app/src/main/java/com/bagicode/loginsatwik/LoginActivity.java
+++ app/src/main/java/com/bagicode/loginsatwik/LoginActivity.java
@@ -1,17 +1,26 @@
 package com.bagicode.loginsatwik;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+
+    private EditText etUsername, etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +29,9 @@ public class LoginActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
+        etUsername = findViewById(R.id.et_username);
+        etPassword = findViewById(R.id.et_password);
+
         Button btnRegister = findViewById(R.id.btn_register);
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +39,49 @@ public class LoginActivity extends AppCompatActivity {
                 Intent goRegister = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(goRegister);
                 finish();
+
+            }
+        });
+
+        FloatingActionButton fabLogin = findViewById(R.id.fab_login);
+        fabLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+
+                if (username.equals("")){
+                    Toast.makeText(LoginActivity.this, "Silahkan input username", Toast.LENGTH_SHORT).show();
+
+                } else if (password.equals("")){
+                    Toast.makeText(LoginActivity.this, "Silahkan input password", Toast.LENGTH_SHORT).show();
+
+                } else {
+
+                    mAuth.signInWithEmailAndPassword(username, password)
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information\
+                                        FirebaseUser user = mAuth.getCurrentUser();
+
+                                        Toast.makeText(LoginActivity.this, "Authentication success.",
+                                                Toast.LENGTH_SHORT).show();
+
+
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+
+                                    // ...
+                                }
+                            });
+
+                }
 
             }
         });
